1c3c8417b565816a6f53b1d2369eff32ef3f9a3f
mounikarallabandi <138767294+mounikarallabandi@users.noreply.github.com>
2024-01-11 12:32:19 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -6,11 +6,5 @@ app = Flask(__name__)
 def hello():
     return 'Hello, Dockerized Python Application!'
 
-# Health check endpoint
-@app.route('/health_check')
-def health_check():
-    return 'OK'
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
